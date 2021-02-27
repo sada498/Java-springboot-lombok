7ddd24e9eb80a61538314a5103216925d71a94a4
sada498 <sada.y@outlook.com>
2021-02-27 21:59:21 +1100

    @Data

--- src/main/java/com/sada/Javaspringbootlambok/model/EntityWithOutLombok.java
+++ src/main/java/com/sada/Javaspringbootlambok/model/EntityWithOutLombok.java
@@ -1,4 +1,12 @@
 package com.sada.Javaspringbootlambok.model;
 
+import lombok.Data;
+
 public class EntityWithOutLombok {
+    @Data(staticConstructor="of")
+    public class Company {
+        private final Person founder;
+        private String name;
+        private List<Person> employees;
+    }
 }
